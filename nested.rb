3abad034f94af09b0a84eb114285bcda012157bd
Samuel W Tong <ssamt1142@gmail.com>
2019-03-06 03:48:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,7 +106,7 @@ def changing_alan
      alans_new_info = "GUI"
      
   programmer_hash[:alan_kay][:known_for] = "GUI"
-  return [:alan_kay][:known_for]
+  return programmer_hash[:alan_kay][:known_for]
 end
 
 def adding_to_dennis
