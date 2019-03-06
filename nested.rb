647330531654ae72c8fcb3ca7239958d19ad2efe
Samuel W Tong <ssamt1142@gmail.com>
2019-03-06 03:33:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,8 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-language = programmer_hash[:dennis_ritchie][:language]
-language.to_s
+programmer_hash[:dennis_ritchie][:language].join
 end
 
 def adding_matz
